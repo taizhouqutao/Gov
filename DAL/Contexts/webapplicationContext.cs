5c7350bbfb859e9dc26eaba9d3f3d68e1247c322
瞿涛 <qutao@wingtech.com>
2025-02-21 15:39:45 +0800

    first commit

---
+++ DAL/Contexts/webapplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Modles;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DAL.Contexts
+{
+    internal class webapplicationContext : DbContext
+    {
+        public webapplicationContext()
+        {
+            configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+        }
+        public DbSet<User> Users { get; set; }
+
+        public IConfiguration configuration;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = configuration.GetConnectionString("Default");
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+    }
+}
---
+++ DAL/Modles/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Modles
+{
+    public class User
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("UserName")]
+        public string UserName { get; set; }
+    }
+}
