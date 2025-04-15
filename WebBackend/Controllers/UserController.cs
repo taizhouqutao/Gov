59ab3996a59228b27ce97d1bfc30a84f0f46beef
瞿涛 <qutao@wingtech.com>
2025-04-15 12:15:08 +0800

    新增MVC后台

---
+++ WebBackend/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebBackend.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
