8301a5dab705e81edf5be7507dc2590a4ee62513
GuEn <guen@Myun.info>
2025-04-25 19:37:07 +0800

    ii

---
+++ DAL/Migrations/20250424145719_UserHead.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    public partial class UserHead : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserEmail",
+                table: "Users",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserHead",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserPost",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserEmail",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserHead",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserPost",
+                table: "Users");
+        }
+    }
+}
