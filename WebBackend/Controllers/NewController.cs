88b6ba49665c3429a668f3c55370793179f5a51b
GuEn <guen@Myun.info>
2025-05-03 17:15:54 +0800

    iii

--- WebBackend/Controllers/NewController.cs
+++ WebBackend/Controllers/NewController.cs
@@ -364,5 +364,59 @@ namespace WebBackend.Controllers
                 };
             }
         }
+    
+        [HttpPost]
+        public ActionResult FileUpload()
+        {
+            try
+            {
+                // 1. 检查是否有文件上传
+                if (Request.Form.Files.Count == 0)
+                {
+                    return Json(new { success = false, message = "未接收到文件" });
+                }
+
+                // 2. 遍历所有上传的文件
+                var uploadedFiles = Request.Form.Files;
+                var savePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Uploads", "New"); // 存储路径
+
+                // 3. 如果目录不存在则创建
+                if (!Directory.Exists(savePath))
+                {
+                    Directory.CreateDirectory(savePath);
+                }
+                var ResPath="/Uploads/New/";
+                string fileName=string.Empty;
+                // 4. 保存每个文件
+                for (int i = 0; i < uploadedFiles.Count; i++)
+                {
+                    var file = uploadedFiles[i];
+                    if (file.Length > 0)
+                    {
+                        fileName = Path.GetFileName(file.FileName);
+                        var fileExt=fileName.Split('.').LastOrDefault();
+                        // 生成随机数作为文件名
+                        var NewFileName=DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                        var random = new Random();
+                        var randomNum = random.Next(1000, 9999);
+                        fileName = $"{NewFileName}{randomNum}.{fileExt}";
+
+                        var filePath = Path.Combine(savePath, fileName);
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            file.CopyTo(stream);
+                        }
+                        break;
+                    }
+                }
+
+                // 5. 返回成功信息
+                return Json(new { success = true, message = "上传成功",url=$"{ResPath}{fileName}" });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "上传失败: " + ex.Message });
+            }
+        }
     }
 }
